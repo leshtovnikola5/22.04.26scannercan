e0d0bc2a15862f7faa4a1a067e08dd9d1b6f1237
leshtovnikola5 <leshtovnikola5@gmail.com>
2026-04-22 14:20:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,24 @@ import easyocr
 from PIL import Image
 import numpy as np
 
-# Заглавие
 st.title("Програма за откриване на вредни съставки")
 
 st.write("Качи снимка на етикет от продукт.")
 
-# Качване на снимка
 file = st.file_uploader("Избери снимка", type=["jpg", "png", "jpeg"])
 
-# Примерни вредни съставки
-harmful = ["E621", "E102", "E110", "E250"]
+harmful = ["E621", "E102", "E110", "E250", "E110", "E122", "E124", "E951", "E954", "E621", "E150d", "E211", "E220", "E228"]
 
 if file is not None:
-    # Показваме снимката
     image = Image.open(file)
     st.image(image, caption="Твоята снимка")
 
     st.write("Чета текста от снимката...")
 
-    # OCR
     reader = easyocr.Reader(['en', 'bg'])
     img = np.array(image)
     result = reader.readtext(img)
 
-    # Събираме текста
     text = ""
     for r in result:
         text += r[1] + " "
@@ -34,7 +28,6 @@ if file is not None:
     st.subheader("Намерен текст:")
     st.write(text)
 
-    # Търсим вредни съставки
     found = []
 
     for item in harmful:
